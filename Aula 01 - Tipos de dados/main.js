5066cfe9c80e7081dc69226b4a92dd6a84fae72f
Nathan Ventura <nathanventura@Mac-mini-de-Nathan.local>
2025-01-11 18:30:12 -0300

    Tipo de dados

---
+++ Aula 01 - Tipos de dados/main.js
@@ -0,0 +1,14 @@
+var nome = "Nathan"
+console.log(typeof(nome));
+
+// Validar se o dado inserido é uma string
+
+var nomeAluno = 25
+if (typeof(nomeAluno) == "string"){
+  console.log(nome.toLocaleUpperCase());
+}else{
+  console.log("Você precisa usar uma string")
+}
+
+// Operador
+
---
+++ Aula 01 - Tipos de dados/node_67c98aac683fe.tmp.js
@@ -0,0 +1,2 @@
+var nome = "Nathan"
+console.log(typeof(nome));
